660d85823f3a256e513d18e9c8fcdf23fcbf9e08
Judith soro Corvo <judithsorocorvo@alumno.ieselrincon.es>
2024-02-01 12:25:30 +0000

    ejercicio tarea6.4

---
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+
+class AdminProductController extends Controller
+{
+    public function index()
+    {
+        $viewData = [];
+        $viewData["title"] = "Admin Page - Products - Online Store";
+        $viewData["products"] = Product::all();
+        return view('admin.product.index')->with("viewData", $viewData);
+    }
+
+
+    function store($request)
+    {
+
+    }
+}
